f2d251d18d39b44ad07d825fd9cccd25527de08b
Anna Abramowicz <42539465+annaabramowicz@users.noreply.github.com>
2025-02-28 11:11:13 +0100

    added UI ContactSection

--- src/app/components/Sections/ContactSection.tsx
+++ src/app/components/Sections/ContactSection.tsx
@@ -1,32 +1,69 @@
+"use client";
+
+import { useEffect } from "react";
 import EmailForm from "../EmailForm";
+import Image from "next/image";
+import Link from "next/link";
 
 const ContactSection = () => {
+  useEffect(() => {
+    const elements = document.querySelectorAll(".scale-in-element");
+
+    const observer = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("animate-scale-in");
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+    elements.forEach((element) => observer.observe(element));
+  }, []);
+
   return (
     <section className="h-fit w-screen px-4 py-12">
       <div className="flex justify-center ">
-        <div className="w-1/3 pr-2">
+        <div className="scale-in-element opacity-0 scale-0 w-1/3 pr-2">
           <h1 className="text-4xl pb-3">Contact</h1>
 
-          <p className="max-w-xl text-lg  pb-3">
+          <p className="max-w-xl text-lg  pb-5">
             If you happen to have any questions about my profile or potential
             cooperation, please feel free to contact me.
           </p>
 
-          <span className="flex items-center gap-2 pb-3">
-            {/* <Image
-                      aria-hidden
-                      src="/file.svg"
-                      alt="File icon"
-                      width={16}
-                      height={16}
-                    /> */}
+          <span className="flex items-center gap-5 pb-5">
+            <Image
+              src="/linkedin.png"
+              alt="linkedin icon"
+              width={40}
+              height={40}
+            />
+            <Link href={"www.linkedin.com/in/annababramowicz"}>
+              linkedin/annaabramowicz
+            </Link>
+          </span>
+
+          <span className="flex items-center gap-5 pb-5">
+            <Image src="/github.png" alt="github icon" width={40} height={40} />
+            <Link href={"https://github.com/annaabramowicz"}>
+              github/annaabramowicz
+            </Link>
+          </span>
+
+          <span className="flex items-center gap-5 pb-5">
+            <Image src="/mail.png" alt="mail icon" width={40} height={40} />
+            anna.b.abramowicz@gmail.com
+          </span>
+
+          <span className="flex items-center gap-5 pb-5">
+            <Image src="/phone.png" alt="phone icon" width={40} height={40} />
             +48 530 728 298
           </span>
         </div>
-
-        <div className="rounded-lg bg-gradient-to-b from-orange-50 to-gray-200 p-8 shadow-lg w-1/3">
-          <EmailForm />
-        </div>
+        <EmailForm />
       </div>
     </section>
   );
