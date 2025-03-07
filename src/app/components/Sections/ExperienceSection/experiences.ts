6fdb7cb68a6a1698cb484d596a5d976deabada90
Anna Abramowicz <42539465+annaabramowicz@users.noreply.github.com>
2025-03-07 13:21:00 +0100

    added task in project

--- src/app/components/Sections/ExperienceSection/experiences.ts
+++ src/app/components/Sections/ExperienceSection/experiences.ts
@@ -3,61 +3,108 @@ export const experiences = [
     title: "Personal Development",
     company: "Self-Development",
     period: "01.2024 - currently",
+    projects: [
+      {
+        title: "Independent Application Development",
+        methodology: "Self-Learning",
+        industry: "Technology",
+        tasks: [
+          "Developed personal projects using modern web technologies.",
+          "Explored new frameworks and libraries to stay up-to-date.",
+          "Built full-stack applications with React, Next.js and Mongop",
+        ],
+      },
+    ],
   },
   {
     title: "Technical Specialist",
     company: "SCD Sebastian Cibis",
     period: "10.2022 - 12.2023",
+    projects: [
+      {
+        title: "Technical Design and Invoicing",
+        methodology: "Traditional",
+        industry: "Construction",
+        tasks: [
+          "Created designs and technical drawings using Autodesk AutoCAD.",
+          "Performed calculations and invoicing in MS Excel.",
+          "Selected insulation covers for industrial equipment based on project requirements, budget, and efficiency.",
+        ],
+      },
+    ],
   },
   {
     title: "Business Owner",
     company: "Short-term rental service in Wroclaw",
     period: "07.2019 - currently",
+    projects: [
+      {
+        title: "Managing Short-Term Rentals",
+        methodology: "Entrepreneurship",
+        industry: "Hospitality",
+        tasks: [
+          "Managed property rentals and guest relations.",
+          "Optimized pricing and occupancy rates to maximize revenue.",
+          "Handled marketing, booking platforms, and customer support.",
+        ],
+      },
+    ],
   },
   {
     title: "Junior Software Developer",
     company: "Capgemini Software Solutions Center",
     period: "09.2017 – 05.2019",
+    projects: [
+      {
+        title: "Project: Lemon / Zurich Insurers (Scrum) - Insurance",
+        methodology: "Scrum",
+        industry: "Insurance",
+        tasks: [
+          "Content migration from CMS Sitecore 8.2 to 9.0.2 with PowerShell.",
+          "Automating migration with PowerShell scripts.",
+          "Participating in code reviews and sprint planning.",
+          "Troubleshooting and optimizing migration processes.",
+        ],
+      },
+      {
+        title: "Project: Salog / Kuehne Nagel (Kanban) - Logistics",
+        methodology: "Kanban",
+        industry: "Logistics",
+        tasks: [
+          "Bug fixing in production and test environments (Java).",
+          "Java 8, Spring Framework, Hibernate, Maven, Git.",
+          "Development of system functionalities based on requests.",
+          "Code reviews, improving code quality, and ensuring best practices.",
+        ],
+      },
+      {
+        title: "Paid Internship: Java Starter Kit",
+        methodology: "N/A",
+        industry: "N/A",
+        tasks: [
+          "Backend solutions with Java, Spring, Hibernate.",
+          "Frontend interfaces with JavaScript and Angular 5.",
+          "Working with MySQL databases and Git tools.",
+          "Testing, debugging, and deploying applications.",
+        ],
+      },
+    ],
   },
   {
     title: "Technical Specialist",
     company: "CS-BETON Poland - Design",
     period: "07.2016 - 08.2017",
-  },
-];
-
-export const capgeminiProjects = [
-  {
-    title: "Project: Lemon / Zurich Insurers (Scrum) - Insurance",
-    methodology: "Scrum",
-    industry: "Insurance",
-    tasks: [
-      "Content migration from CMS Sitecore 8.2 to 9.0.2 with PowerShell.",
-      "Automating migration with PowerShell scripts.",
-      "Participating in code reviews and sprint planning.",
-      "Troubleshooting and optimizing migration processes.",
-    ],
-  },
-  {
-    title: "Project: Salog / Kuehne Nagel (Kanban) - Logistics",
-    methodology: "Kanban",
-    industry: "Logistics",
-    tasks: [
-      "Bug fixing in production and test environments (Java).",
-      "Java 8, Spring Framework, Hibernate, Maven, Git.",
-      "Development of system functionalities based on requests.",
-      "Code reviews, improving code quality, and ensuring best practices.",
-    ],
-  },
-  {
-    title: "Paid Internship: Java Starter Kit",
-    methodology: "N/A",
-    industry: "N/A",
-    tasks: [
-      "Backend solutions with Java, Spring, Hibernate.",
-      "Frontend interfaces with JavaScript and Angular 5.",
-      "Working with MySQL databases and Git tools.",
-      "Testing, debugging, and deploying applications.",
+    projects: [
+      {
+        title: "Prefabricated Concrete Drainage Design",
+        methodology: "Traditional",
+        industry: "Construction",
+        tasks: [
+          "Designed and selected prefabricated concrete slot drainage systems for airport investments and container terminals.",
+          "Utilized Autodesk AutoCAD for technical drawings and designs.",
+          "Ensured compliance with industry standards and regulations.",
+        ],
+      },
     ],
   },
 ];
