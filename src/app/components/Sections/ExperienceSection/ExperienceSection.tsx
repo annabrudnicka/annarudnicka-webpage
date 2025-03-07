cdb646d20cafb87eabe6e4977d9d8b9cd1ebe5a1
Anna Abramowicz <42539465+annaabramowicz@users.noreply.github.com>
2025-03-07 10:50:19 +0100

    popup instead of hover in experience section

--- src/app/components/Sections/ExperienceSection/ExperienceSection.tsx
+++ src/app/components/Sections/ExperienceSection/ExperienceSection.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import { useState } from "react";
 import Image from "next/image";
 import { capgeminiProjects, experiences } from "./experiences";
 import useIntersectionObserver from "@/app/hooks/useIntersectionObserver";
 
 const ExperienceSection = () => {
   useIntersectionObserver("scale-in-element");
+  const [activePopup, setActivePopup] = useState(null);
+
+  const togglePopup = (index) => {
+    setActivePopup(activePopup === index ? null : index);
+  };
 
   return (
     <section className="h-fit w-screen bg-gradient-bg flex items-center">
@@ -16,63 +22,78 @@ const ExperienceSection = () => {
           {experiences.map((exp, index) => (
             <div
               key={index}
-              className="scale-in-element relative group bg-white p-4 rounded-xl shadow-md h-40"
+              className="scale-in-element relative bg-white rounded-xl shadow-md h-40"
             >
               {exp.company.includes("Capgemini") ? (
-                <div
-                  className="group relative overflow-hidden transition-transform duration-300 
-                hover:w-[200%] hover:h-[482%] hover:z-50 z-0 hover:-translate-x-[480px] hover:-translate-y-72"
-                >
-                  <div className="absolute inset-0 transition-all duration-300 opacity-0 group-hover:opacity-100">
-                    <div className="bg-white border border-gray-400 rounded-lg p-4 mx-auto space-y-4">
-                      {capgeminiProjects.map((project, index) => (
-                        <div
-                          key={index}
-                          className="bg-gray-100 p-4 rounded-lg shadow-lg "
+                <div className="border border-gray-600 rounded-xl p-4">
+                  <div className="flex">
+                    <Image
+                      src="/Capgemini.svg"
+                      alt="Capgemini icon"
+                      width={70}
+                      height={70}
+                      className="object-contain mr-6"
+                    />
+                    <div className="flex-1">
+                      <h3 className="font-playfair text-3xl">{exp.title}</h3>
+                      <h4 className="text-md text-gray-600">
+                        {exp.company} • {exp.period}
+                      </h4>
+                      <div className="flex justify-end w-full">
+                        <button
+                          className="bg-gray-200 px-4 py-2 rounded-lg mt-2"
+                          onClick={() => togglePopup(index)}
                         >
-                          <h2 className="font-playfair text-3xl w-[950px] mb-2">
-                            {project.title}
-                          </h2>
-                          <ul className="list-disc list-inside space-y-1">
-                            {project.tasks.map((task, taskIndex) => (
-                              <li key={taskIndex}>{task}</li>
-                            ))}
-                          </ul>
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-
-                  <div className="group-hover:opacity-0 transition-all duration-300 opacity-100">
-                    <div className="flex ">
-                      <Image
-                        src="/Capgemini.svg"
-                        alt="Capgemini icon"
-                        width={70}
-                        height={70}
-                        className="object-contain mr-6"
-                      />
-
-                      <div className="flex-1">
-                        <h3 className="font-playfair text-3xl">{exp.title}</h3>
-                        <h4 className="text-md text-gray-600">
-                          {exp.company} • {exp.period}
-                        </h4>
-                        <div className="flex justify-end w-full">
-                          <button className="bg-gray-200 w-max ">
-                            Show more
-                          </button>
-                        </div>
+                          Show more
+                        </button>
                       </div>
                     </div>
                   </div>
+                  {activePopup === index && (
+                    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+                      <div className="bg-white border border-gray-400 rounded-lg p-3 max-w-2xl relative">
+                        <button
+                          className="absolute top-2 right-4 "
+                          onClick={() => setActivePopup(null)}
+                        >
+                          &times;
+                        </button>
+                        <h2 className="font-playfair text-3xl mb-4">
+                          Capgemini Projects
+                        </h2>
+                        {capgeminiProjects.map((project, index) => (
+                          <div
+                            key={index}
+                            className="bg-gray-100 p-4 rounded-lg shadow-lg my-1"
+                          >
+                            <h3 className="font-playfair text-2xl mb-2">
+                              {project.title}
+                            </h3>
+                            <ul className="list-disc list-inside space-y-1">
+                              {project.tasks.map((task, taskIndex) => (
+                                <li key={taskIndex}>{task}</li>
+                              ))}
+                            </ul>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  )}
                 </div>
               ) : (
-                <div>
+                <div className="p-4">
                   <h3 className="font-playfair text-3xl mb-1">{exp.title}</h3>
                   <h4 className="text-md text-gray-600">
                     {exp.company} • {exp.period}
                   </h4>
+                  <div className="flex justify-end w-full">
+                    <button
+                      className="bg-gray-200 px-4 py-2 rounded-lg mt-2"
+                      onClick={() => togglePopup(index)}
+                    >
+                      Show more
+                    </button>
+                  </div>
                 </div>
               )}
             </div>
--- src/app/components/Sections/HomeSection/Header.tsx
+++ src/app/components/Sections/HomeSection/Header.tsx
@@ -5,10 +5,10 @@ const Header = () => {
   return (
     <header>
       <div className="flex justify-between items-center py-4">
-        <span className="text-left w-[276px]">tel.: +48 530 728 298</span>
+        <span className="text-left w-[276px]">+48 530 728 298</span>
         <DownloadCVButton />
         <span className="text-right w-[350px]">
-          email: anna.b.abramowicz@gmail.com
+          anna.b.abramowicz@gmail.com
         </span>
       </div>
       <span className="block h-[0.5px] w-[120vw] -ml-10 bg-zinc-300"></span>
