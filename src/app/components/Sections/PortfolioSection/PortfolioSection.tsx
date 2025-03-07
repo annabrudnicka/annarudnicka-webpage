6e03684df11717d26009ccf4ead1aa3620c1785e
Anna Abramowicz <42539465+annaabramowicz@users.noreply.github.com>
2025-03-07 14:03:26 +0100

    reorganization buttons demo and code in portfolio section

--- src/app/components/Sections/PortfolioSection/PortfolioSection.tsx
+++ src/app/components/Sections/PortfolioSection/PortfolioSection.tsx
@@ -16,57 +16,81 @@ const PortfolioSection = () => {
 
           <div className="grid grid-cols-2 gap-x-12 ">
             <div className="scale-in-element">
-              <h3 className="font-playfair  mb-3 text-center">Food Planner</h3>
-              <p className="text-md text-gray-600 mb-3">
+              <h3 className="font-playfair  mb-3 text-3xl text-center">
+                Food Planner
+              </h3>
+              <p className="text-md text-gray-600 ">
                 Search, find, and discover interesting recipes. Easily compose
                 your shopping list by searching for ingredients and adding them
                 to your fridge. Data is fetched from an external API and
                 prepared accordingly.
               </p>
+              <div className="flex justify-center gap-4 mb-4">
+                <Link
+                  href="https://food-planner-2024.vercel.app"
+                  target="_blank"
+                >
+                  <button className="bg-gradient-bg hover:bg-neutral-600 border-gray-400 transition-all transform hover:scale-105">
+                    Demo
+                  </button>
+                </Link>
+                <Link
+                  href="https://github.com/annaabramowicz/food-planner-2024"
+                  target="_blank"
+                >
+                  <button className="bg-gradient-bg hover:bg-neutral-600 border-gray-400 transition-all transform hover:scale-105">
+                    Code
+                  </button>
+                </Link>
+              </div>
             </div>
-            <div className="scale-in-element">
-              <h3 className="font-playfair  mb-3 text-center">
-                Eisenhower Matrix
-              </h3>
-              <p className="text-md text-gray-600 mb-3">
-                Manage your tasks effectively by categorizing them based on
-                urgency and importance. The Eisenhower Matrix helps you
-                prioritize your daily responsibilities with a simple interface.
-              </p>
+            <div className="scale-in-element flex flex-col justify-between">
+              <div>
+                <h3 className="font-playfair mb-3 text-3xl text-center">
+                  Eisenhower Matrix
+                </h3>
+                <p className="text-md text-gray-600 mb-3">
+                  Manage your tasks effectively by categorizing them based on
+                  urgency and importance. The Eisenhower Matrix helps you
+                  prioritize your daily responsibilities with a simple
+                  interface.
+                </p>
+              </div>
+
+              <div className="flex justify-center gap-4 mb-4">
+                <Link
+                  href="https://food-planner-2024.vercel.app"
+                  target="_blank"
+                >
+                  <button className="bg-gradient-bg hover:bg-neutral-600 border-gray-400 transition-all transform hover:scale-105">
+                    Demo
+                  </button>
+                </Link>
+                <Link
+                  href="https://github.com/annaabramowicz/food-planner-2024"
+                  target="_blank"
+                >
+                  <button className="bg-gradient-bg hover:bg-neutral-600 border-gray-400 transition-all transform hover:scale-105">
+                    Code
+                  </button>
+                </Link>
+              </div>
             </div>
-            <div className="scale-in-element my-7">
-              <article className="overflow-hidden rounded-2xl shadow-xl transition-transform transform hover:scale-105 hover:shadow-2xl border border-gray-300 w-96 mx-auto">
-                <div className="group relative  h-44  overflow-hidden rounded-2xl">
+
+            <div className="scale-in-element ">
+              <article className="overflow-hidden rounded-2xl shadow-xl border border-gray-300 w-96 mx-auto">
+                <div className="group relative h-44 rounded-2xl">
                   <Image
                     alt="Food Planner"
                     src="/foodPlannerImage.png"
                     width={1000}
                     height={1000}
-                    className=" w-full object-cover transition-opacity group-hover:opacity-75"
+                    className="w-full object-cover"
                   />
-
-                  <div className="absolute inset-0 flex items-center justify-center gap-8 opacity-0 transition-all transform group-hover:opacity-100 group-hover:translate-y-0 translate-y-8">
-                    <Link
-                      href="https://food-planner-2024.vercel.app"
-                      target="_blank"
-                    >
-                      <button className="bg-neutral-700 hover:bg-neutral-600 text-white  transition-all transform hover:scale-105">
-                        Demo
-                      </button>
-                    </Link>
-                    <Link
-                      href="https://github.com/annaabramowicz/food-planner-2024"
-                      target="_blank"
-                    >
-                      <button className="bg-neutral-700 hover:bg-neutral-600 text-white transition-all transform hover:scale-105">
-                        Code
-                      </button>
-                    </Link>
-                  </div>
                 </div>
               </article>
 
-              <div className="flex justify-center flex-wrap gap-4 mt-7">
+              <div className="flex justify-center flex-wrap gap-3 mt-3">
                 {portfolioTechnologies.FoodPlanner.map((tech) => (
                   <div
                     key={tech.name}
@@ -86,39 +110,20 @@ const PortfolioSection = () => {
                 ))}
               </div>
             </div>
-            <div className="scale-in-element my-7">
-              <article className="overflow-hidden rounded-2xl shadow-xl transition-transform transform hover:scale-105 hover:shadow-2xl border border-gray-300 w-96 mx-auto">
+            <div className="scale-in-element">
+              <article className=" rounded-2xl shadow-xl border border-gray-300 w-96 mx-auto">
                 <div className="group relative block h-44 overflow-hidden rounded-2xl">
                   <Image
                     alt="Eisenhower Matrix"
                     src="/eisenhowerMatrixImage.png"
                     width={1000}
                     height={1000}
-                    className="absolute inset-0 h-full w-full object-cover transition-opacity group-hover:opacity-75"
+                    className="absolute inset-0 h-full w-full object-cover"
                   />
-
-                  <div className="absolute inset-0 flex items-center justify-center gap-8 opacity-0 transition-all transform group-hover:opacity-100 group-hover:translate-y-0 translate-y-8">
-                    <Link
-                      href="https://eisenhower-matrix-2024.vercel.app"
-                      target="_blank"
-                    >
-                      <button className="bg-neutral-700 hover:bg-neutral-600 text-white  transition-all transform hover:scale-105">
-                        Demo
-                      </button>
-                    </Link>
-                    <Link
-                      href="https://github.com/annaabramowicz/eisenhower-matrix"
-                      target="_blank"
-                    >
-                      <button className="bg-neutral-700 hover:bg-neutral-600 text-white transition-all transform hover:scale-105">
-                        Code
-                      </button>
-                    </Link>
-                  </div>
                 </div>
               </article>
 
-              <div className="flex justify-center flex-wrap gap-4 mt-7">
+              <div className="flex justify-center flex-wrap gap-3 mt-3">
                 {portfolioTechnologies.EisenhowerMatrix.map((tech) => (
                   <div
                     key={tech.name}
