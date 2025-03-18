d67ea84b4902df7714568baa371e8ad6d1f52b4a
Anna Abramowicz <42539465+annaabramowicz@users.noreply.github.com>
2025-03-18 12:19:15 +0100

    added overflow-x to body because of hamburger menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,9 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`
-      ${montserrat.className} 
-      ${playfair.variable}`}
+        className={`${montserrat.className}${playfair.variable} overflow-x-hidden`}
       >
         {children}
       </body>
